d7e4bae602f6eb5b4d15dc34ab2e8d2c5d04e01e
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2015-01-30 15:35:48 +0000

    check unmounted before component mixin onChange listener is invoked

--- src/Store.js
+++ src/Store.js
@@ -33,7 +33,7 @@ class Store {
         if(!this.listener){
             warn("A change handler is missing from a component with a McFly mixin. Notifications from Stores are not being handled.");
         }
-        this.listener && self.addChangeListener(this.listener);
+        this.listener && self.addChangeListener(()=>( this._lifeCycleState != "UNMOUNTED" ) && this.listener());
       },
       componentWillUnmount: function() {
         this.listener && self.removeChangeListener(this.listener);
